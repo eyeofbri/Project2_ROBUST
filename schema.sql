3a93effd3ebfe609426a2704b3027a0a9c2bd51a
Skokiaan <skokiaan@aol.com>
2017-05-04 21:07:01 -0400

    Create DB and tables

---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE tickit_db;
+
+USE tickit_db;
+
+CREATE TABLE users
+(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	name VARCHAR (255) NOT NULL,
+	email VARCHAR(255),
+	password VARCHAR (255),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE tasks
+(
+	id INTEGER NOT NULL AUTO_INCREMENT,
+	task VARCHAR(255) NOT NULL,
+	description VARCHAR(255),
+	complete BOOLEAN DEFAULT false,
+	created_by VARCHAR(255) NOT NULL,
+	assigned_to VARCHAR(255),
+	date_created DATE,
+	date_due DATE,
+	category VARCHAR(255) DEFAULT "Work",
+	drawing LONGBLOB,
+	link VARCHAR (255),
+	PRIMARY KEY(id)
+);
