ad461281789bc0c8a45964a23feec42856c2f010
Aparna Thirthapura <aparnathirthapura@Aparnas-MacBook-Air.local>
2017-05-10 23:30:29 -0400

    Handlebars

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -3,11 +3,15 @@ var db = require("../models");
 
 module.exports = function(app){
 	app.get("/", function(req, res){
-		res.sendFile(path.join(__dirname + "/../public/login.html"));
+		res.render("home");
 	});
 
-	app.get("/addTask", function(req,res){
-		res.sendFile(path.join(__dirname + "/../public/main.html"));
+	app.get("/login", function(req, res){
+		res.render("login");
+	});
+
+	app.get("/signup", function(req, res){
+		res.render("signup");
 	});
 
 	app.post("/login", function(req, res){
@@ -15,20 +19,30 @@ module.exports = function(app){
 	    db.User.findOne({where:{userEmail:req.body.email}}).then(function(dbUser){
 	      console.log(dbUser);
 				if (dbUser === null) {
-					res.sendFile(path.join(__dirname + "/../public/login.html"));
+					res.render("login")
 				}
 				else {
-					if(dbUser.userPassword === req.body.psw) {
-						res.sendFile(path.join(__dirname + "/../public/main.html"));
+					if(dbUser.userPassword === req.body.password) {
+						res.render("task")
 					} else {
-						res.sendFile(path.join(__dirname + "/../public/login.html"));
+						res.render("login")
 					}
 				}
 	    });
   	});
 
-  app.post("/addTask", function(req, res){
-    console.log(req.body);
-  });
+		app.post("/signup", function(req, res){
+		    console.log(req.body);
+		    db.User.create({
+					userName:req.body.name,
+					userEmail:req.body.email,
+					userPassword:req.body.password
+				}).then(function(dbUser){
+		      console.log(dbUser);
+				res.render("task")
+		    });
+	  	});
+
+
 
 };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
 
 var port = process.env.PORT || 3000;
 
@@ -12,10 +13,13 @@ var db = require("./models");
 
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 require("./routes/allRoutes.js")(app);
 
